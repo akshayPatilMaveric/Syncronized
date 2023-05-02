108c5dc4da0d2b134ccf53684d27da344a899ef3
akshaypa <akshaypa@maveric-systems.com>
2023-05-02 19:57:08 +0530

    Added the Tests and passing parameters using testng.xml

--- src/test/java/org/example/test.java
+++ src/test/java/org/example/test.java
@@ -1,4 +1,27 @@
 package org.example;
 
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
 public class test {
+    Smain login;
+    @BeforeTest
+    public void launch(){
+        login=new Smain();
+        login.launchbrowser();
+    }
+
+    @Test
+    @Parameters({"Username"})
+    public void logink(String Username){
+        login.login(Username);
+    }
+
+    @AfterTest
+    public void Quit(){
+        login.quit();
+    }
+
 }

ee012ec23c73c745149a73ee5a859d6cf01eea55
Manjunath S C P <manjunathsc@mavltmacc0822066.bbrouter>
2023-05-05 11:36:34 +0530

    Added Synchronised Method

--- src/main/java/org/example/DP.java
+++ src/main/java/org/example/DP.java
@@ -2,38 +2,55 @@ package org.example;
 
 import org.testng.annotations.DataProvider;
 
-import java.util.Dictionary;
-import java.util.Hashtable;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DP {
-    public Dictionary<String,String> usernameStatus=new Hashtable<>();
-    @DataProvider(name = "Usernames")
-    public Object[][] dpUsernames(){
+    public Map<String, String> usernameStatus = new HashMap<>();
+
+    @DataProvider(name = "Usernames", parallel = true)
+    public Object[][] dpUsernames() {
         return new Object[][] {
-                {"standard_user","secret_sauce",3},
-                {"problem_user","secret_sauce",10},
-                {"standard_user","secret_sauce",4}
+                { "standard_user", "secret_sauce", 3 },
+                { "problem_user", "secret_sauce", 10 },
+                { "standard_user", "secret_sauce", 4 }
         };
     }
-    public DP(){
-        usernameStatus.put("standard_user","open");
-        usernameStatus.put("problem_user","open");
+
+    public void lockUsername(String username) {
+        long threadId = Thread.currentThread().getId();
+        if (checkUsernameStatus(username)) {
+            usernameStatus.put(username, "locked");
+        } else {
+            while(!checkUsernameStatus(username)) {
+                System.out.println(threadId + " Waiting for the user to be available " + username);
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        System.out.println(threadId + " Locked user name " + username);
     }
-    public void lockUsername(String Username){
-        usernameStatus.remove(Username);
-        usernameStatus.put(Username,"locked");
+
+    public void unlockUsername(String username) {
+        long threadId = Thread.currentThread().getId();
+        usernameStatus.put(username, "unlocked");
+        System.out.println(threadId + " Unlocked user name " + username);
     }
-    public void unlockUsername(String Username){
-        usernameStatus.remove(Username);
-        usernameStatus.put(Username,"open");
-    }
-    public boolean checkUsernameStatus(String Username){
-        if(usernameStatus.get(Username).equals("open")){
+
+    public synchronized boolean checkUsernameStatus(String username) {
+        if (usernameStatus.containsKey(username)) {
+            if (usernameStatus.get(username).equals("unlocked")) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
             return true;
         }
-        else{
-            return false;
-        }
+
     }
 
 }
--- src/test/java/org/example/test.java
+++ src/test/java/org/example/test.java
@@ -1,35 +1,43 @@
 package org.example;
 
+import org.testng.ITestContext;
 import org.testng.annotations.*;
 
 public class test {
     Smain login;
     DP dataPro;
-    @BeforeTest
-    public void launch(){
-        dataPro=new DP();
-        login=new Smain();
-        login.launchbrowser();
+
+    @BeforeSuite
+    public void launch() {
+        dataPro = new DP();
     }
 
-    @Test (dataProvider = "Usernames", dataProviderClass = org.example.DP.class)
-    public void logink(String Username,String Password, Integer qty) throws InterruptedException {
-        if(true){
-            if(dataPro.checkUsernameStatus(Username)){
-                dataPro.lockUsername(Username);
-                login.login(Username,Password,qty);
-                dataPro.unlockUsername(Username);
-            }
-        }
-        else {
-            System.out.println("Username already in use, waiting for next 30 seconds");
-            Thread.sleep(30000);
-        }
+    @BeforeMethod
+    public void beforeTest(Object[] testArgs) {
+        long threadId = Thread.currentThread().getId();
+        String username = (String) testArgs[0];
+        System.out.println(threadId + " myParam value in Before Test: " + username);
+        dataPro.lockUsername(username);
+    }
 
+    @AfterMethod
+    public void afterTest(Object[] testArgs) {
+        long threadId = Thread.currentThread().getId();
+        String username = (String) testArgs[0];
+        System.out.println(threadId + " myParam value in After Test: " + username);
+        dataPro.unlockUsername(username);
+    }
+
+    @Test(dataProvider = "Usernames", dataProviderClass = org.example.DP.class)
+    public void logink(String Username, String Password, Integer qty) throws InterruptedException {
+        long threadId = Thread.currentThread().getId();
+        System.out.println(threadId + " Starting Test with username " + Username);
+        Thread.sleep(20000);
+        System.out.println(threadId + " Ending Test with username " + Username);
     }
 
     @AfterTest
-    public void Quit(){
+    public void Quit() {
         login.quit();
     }
 

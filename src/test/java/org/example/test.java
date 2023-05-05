c58eec7fec20d7720e4701dbbf5b10523693a277
akshaypa <akshaypa@maveric-systems.com>
2023-05-05 11:46:11 +0530

    Changed the if loop

--- src/test/java/org/example/test.java
+++ src/test/java/org/example/test.java
@@ -14,20 +14,15 @@ public class test {
 
     @Test (dataProvider = "Usernames", dataProviderClass = org.example.DP.class)
     public void logink(String Username,String Password, Integer qty) throws InterruptedException {
-        if(true){
-            if(dataPro.checkUsernameStatus(Username)){
+        if(dataPro.checkUsernameStatus(Username)){
                 dataPro.lockUsername(Username);
                 login.login(Username,Password,qty);
                 dataPro.unlockUsername(Username);
-            }
-        }
-        else {
+        }else {
             System.out.println("Username already in use, waiting for next 30 seconds");
             Thread.sleep(30000);
         }
-
     }
-
     @AfterTest
     public void Quit(){
         login.quit();
